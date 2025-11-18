794728aa636e59faf58652d4fdad52352e5788e3
Dhairya Bhadani <dhairyabhadani8092@gmail.com>
2025-11-18 23:02:04 +0530

    chore: initial BankX spring-boot skeleton with health endpoint

---
+++ src/main/java/com/bankx/api/BankxApplication.java
@@ -0,0 +1,13 @@
+package com.bankx.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankx/api/BankxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankx.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
